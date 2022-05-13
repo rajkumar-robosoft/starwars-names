0ec8de4db1bcd1e92c28371f841dbf5c1168a8f3
“rajkumar-robosoft” <rajkumar.singh@robosoftin.com>
2022-05-13 21:19:25 +0530

    Adding the Library

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import uniqueRandomArray from 'unique-random-array';
+import starWarsNames from './starwars-names.json';
+
+
+
+module.exports = {
+  all: starWarsNames,
+  random: uniqueRandomArray(starWarsNames)
+};
+
